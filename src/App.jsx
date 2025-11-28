8645c8dd03f2b7b33ee8a314806666dcdf849ce9
TISEPSE <titi21player@gmail.com>
2025-11-28 23:36:43 +0100

    Ajout formulaire et logique de récupération des inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from './pages/Home.jsx';
 import Login from './pages/Login.jsx';
+import Resgister from './pages/Register.jsx';
+import Forgot_password from "./pages/Forgot_password.jsx"
 import "./static/App.css";
+
 
 function App() {
 
@@ -9,12 +12,14 @@ function App() {
     <BrowserRouter>
     {/* Navigation temporaire, vers la page racine "/" et La page "./pages/Page_Teste.jsx" */}
       <nav>
-        <Link to="/">Home</Link> | <Link to="/login_form">Login</Link>
+        <Link to="/">Home</Link> | <Link to="/register_form">Register</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/login_form" element={<Login/>} />
+        <Route path="/register_form" element={<Resgister/>}/>
+        <Route path="/login_form" element={<Login/>}/>
+        <Route path="/fogot_password" element={<Forgot_password/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Forgot_password.jsx
@@ -0,0 +1,41 @@
+export default function Forgot_password() {
+  return (
+    <main className="w-full h-screen flex items-center justify-center bg-gray-50">
+      <div className="w-full space-y-6 text-gray-600 sm:max-w-md">
+        <div className="text-center">
+          <div className="mt-5 space-y-2">
+            <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">
+              Mot de passe oublié
+            </h3>
+            
+          </div>
+        </div>
+
+        <div className="bg-white shadow p-4 py-6 sm:p-6 sm:rounded-lg text-left">
+          <form
+            className="space-y-5"
+            action="http://localhost:5000/login_form"
+            method="POST"
+          >
+            <div className="flex flex-col items-start">
+              <label className="font-medium block text-left">Email</label>
+              <input
+                type="email"
+                name="email"
+                required
+                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border border-gray-300 focus:border-indigo-600 shadow-sm rounded-lg"
+              />
+            </div>
+
+            <button
+              className="w-full px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
+              type="submit"
+            >
+             Envoyer un lien par mail
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+  )
+}
