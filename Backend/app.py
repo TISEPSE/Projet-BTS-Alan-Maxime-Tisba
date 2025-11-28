ac283a8e93c44065086b30d1ff2ff15d847d7294
TISEPSE <titi21player@gmail.com>
2025-11-28 16:13:13 +0100

    ajout tailwind et teste flask

--- Backend/app.py
+++ Backend/app.py
@@ -1,24 +1,11 @@
-from flask import Flask, request
-from flask_cors import CORS
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-CORS(app)
 
-# récupérer les informations du formulaire quand il POST sur l'endpoint /register_form
-@app.route("/register_form", methods=["POST"])
-def register_form():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    nom = request.form.get("nom")
-    print(f"La page de Register à récupérer => Nom: {nom} Email: {email}, Password: {password}", flush=True)
-    return "OK"
-
-@app.route("/login_form", methods=["POST"])
+@app.route("/login", methods=["POST"])
 def login_Form():
-    email = request.form.get("email")
+    
+    email = request.form.get("name")
     password = request.form.get("password")
-    print(f"La page de Login à récupérer => Email: {email}, Password: {password}", flush=True)
-    return "OK"
 
-if __name__ == "__main__":
-    app.run(port=5000)
+    print(f"Email: {email}, Password: {password}")
