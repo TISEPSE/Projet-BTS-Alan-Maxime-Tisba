c6791bf41814383b460ae18f356ca7b4654a2fc4
TISEPSE <titi21player@gmail.com>
2025-11-29 12:21:28 +0100

    Script powershell fonctionne ( cette fois ci c'est bon )

--- install_Windows.ps1
+++ install_Windows.ps1
@@ -5,10 +5,14 @@ if (!(Test-Path "venv")) {
 
 . .\venv\Scripts\Activate.ps1
 
-pip install --upgrade pip
-pip install flask
+.\venv\Scripts\python.exe -m pip install --upgrade pip
+
+if (Test-Path "requirements.txt") {
+    .\venv\Scripts\python.exe -m pip install -r requirements.txt
+}
 
 npm install
 
-npm run dev:all
+npm run dev:win
+
 Write-Host "Installation terminée. Vous pouvez maintenant exécuter l'application."
