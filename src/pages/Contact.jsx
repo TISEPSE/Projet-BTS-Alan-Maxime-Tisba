cd4b737a45b9237f737aa2533a144ad7b62bb89f
BibiFinance <pivetalanpro@gmail.com>
2025-12-04 18:53:48 +0100

    page de contact maj

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,16 +8,18 @@ export default function Login() {
   );
 
   return (
-    <main className="w-full h-screen flex items-center justify-center bg-gray-50">
+    <main className="w-full h-screen flex items-center justify-center white">
       <div className="w-full space-y-6 text-gray-600 sm:max-w-md">
         <div className="text-center">
-          <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">Se connecter</h3>
+          <h3 className="text-gray-800 text-2xl font-bold sm:text-3xl">Formulaire de Contact</h3>
         </div>
         <div className="bg-white shadow p-4 py-6 sm:p-6 sm:rounded-lg text-left">
           <form className="space-y-5" onSubmit={handleSubmit}>
+            <input name="name" type="text" required placeholder="Nom/Prenom" className="w-full px-3 py-2 border rounded-lg" />
             <input name="email" type="email" required placeholder="Email" className="w-full px-3 py-2 border rounded-lg" />
-            <input name="password" type="password" required placeholder="Mot de passe" className="w-full px-3 py-2 border rounded-lg" />
-            <button type="submit" className="w-full bg-indigo-600 text-white py-2 rounded-lg">Se connecter</button>
+            <input name="Telephone" type="tel" required placeholder="Téléphone" className="w-full px-3 py-2 border rounded-lg" inputMode="numeric" onInput={e => e.target.value = e.target.value.replace(/\D/g,"")} />
+            <input name="Message" type="Message" required placeholder="Message" className="w-full px-3 py-2 border rounded-lg" />
+            <button type="submit" className="w-full bg-indigo-600 text-white py-2 rounded-lg">Submit</button>
           </form>
         </div>
       </div>
